10141d847e61a2c78cc1a212f35ef3589938ba8c
Lucas Possatti <lucas_possatti@hotmail.com>
2016-10-01 14:12:44 -0300

    Improve the Quickstart project

--- quickstart/conf.lua
+++ quickstart/conf.lua
@@ -2,7 +2,7 @@
 
 function love.conf(t)
   t.identity = nil                    -- The name of the save directory (string)
-  -- t.version = "0.9.1"                -- The LÖVE version this game was made for (string)
+  t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
  
   t.window.title = "Untitled"         -- The window title (string)
   t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
--- quickstart/main.lua
+++ quickstart/main.lua
@@ -1,10 +1,36 @@
 
 function love.load()
+  -- Load images.
+  -- img = love.graphics.newImage('assets/foobar.png')
+
+  -- Load music/sounds.
+  -- music = love.audio.newSource("assets/foorocks.ogg")
+  -- music:setVolume(0.8)
+  -- music:setLooping(true)
+  -- music:rewind()
+  -- music:play()
+
+  -- Load fonts.
+  bigFont = love.graphics.newFont(40)
+
+  -- Set up quads.
+  -- quad = love.graphics.newQuad(0, 0, 32, 32, 128, 128)
+
+  -- Set up mouse.
+  -- love.mouse.setVisible(true)
+  -- love.mouse.setGrabbed(true)
 end
 
 function love.update(dt)
+  -- Quit game.
+  if love.keyboard.isDown('escape', 'q') then
+    love.event.push('quit')
+  end
 end
 
 function love.draw()
-  love.graphics.print("Hello World!", 400, 300)
+  love.graphics.setBackgroundColor(190, 190, 190, 255)
+  love.graphics.setColor(0, 145, 255, 255)
+  love.graphics.setFont(bigFont)
+  love.graphics.print("Hello World!", love.graphics.getWidth()/2-120, love.graphics.getHeight()/2-50)
 end
